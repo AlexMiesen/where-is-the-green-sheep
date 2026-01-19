9316b2d8cbae1db9afa854e3c5544e915e3a69de
Alexander Miesen <alexander.miesen@3plearning.com>
2026-01-19 11:37:34 +1100

    Added keyboard movements for player

--- sheep/player.gd
+++ sheep/player.gd
@@ -1,6 +1,7 @@
 extends Sprite2D
 
-var velocity = Vector2(500, 500)
+var velocity := Vector2(0, 0)
+var max_speed := 600.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,5 +10,16 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	position = lerp(position, get_local_mouse_position(), 2 * delta)
-	rotation = velocity.angle()
+	var direction := Vector2(0,0)
+	
+	direction.x = Input.get_axis("move_left", "move_right")
+	direction.y = Input.get_axis("move_up", "move_down")
+
+	position += velocity * delta
+	
+	velocity = direction * max_speed
+	if direction.length() > 1.0:
+		direction.normalized()
+	
+	if direction.length() > 0.0:
+		rotation = velocity.angle()
