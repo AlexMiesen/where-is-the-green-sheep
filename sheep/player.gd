e2c0252be0aa60b2670be6915792209758ea2a11
Alexander Miesen <alexander.miesen@3plearning.com>
2026-01-19 20:54:32 +1100

    Moved code around to make more sense

--- sheep/player.gd
+++ sheep/player.gd
@@ -15,11 +15,10 @@ func _process(delta: float) -> void:
 	direction.x = Input.get_axis("move_left", "move_right")
 	direction.y = Input.get_axis("move_up", "move_down")
 
-	position += velocity * delta
+	if direction.length() > 1.0:
+		direction = direction.normalized()
 	
 	velocity = direction * max_speed
-	if direction.length() > 1.0:
-		direction.normalized()
-	
+	position += velocity * delta
 	if direction.length() > 0.0:
 		rotation = velocity.angle()
