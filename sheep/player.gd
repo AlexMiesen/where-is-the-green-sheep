9555731ce081783187e6d126a8a3fa3fad8bea43
Alexander Miesen <alexander.miesen@3plearning.com>
2026-01-26 21:29:18 +1100

    Smoothed the movement of the sheep by adding steering behaviour

--- sheep/player.gd
+++ sheep/player.gd
@@ -5,6 +5,8 @@ var normal_speed := 600.0
 var max_speed := normal_speed
 var boost_speed := 1500.0
 
+var steering_factor := 10.0
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -12,7 +14,7 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	var direction := Vector2(0,0)
+	var direction := Vector2(0, 0)
 	
 	direction.x = Input.get_axis("move_left", "move_right")
 	direction.y = Input.get_axis("move_up", "move_down")
@@ -24,8 +26,12 @@ func _process(delta: float) -> void:
 		max_speed = boost_speed
 		get_node("Timer").start()
 		
-	velocity = direction * max_speed
+	var desired_velocity := max_speed * direction
+	var steering_vector := desired_velocity - velocity
+	
+	velocity += steering_vector * steering_factor * delta
 	position += velocity * delta
+	
 	if direction.length() > 0.0:
 		rotation = velocity.angle()
 
