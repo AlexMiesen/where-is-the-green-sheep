715a8853efe90a89211c6f575ada20f5f4b02d18
Alexander Miesen <alexander.miesen@3plearning.com>
2026-02-25 21:19:50 +1100

    number of items spawned can only be 6 at a time

--- assets/spawn_random_item.gd
+++ assets/spawn_random_item.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 var items := [preload("gem.tscn"), preload("health_pack.tscn")]
+var spawned_items_count := 0
 
 func _ready():
 	get_node("Timer").timeout.connect(_on_timer_timeout)
@@ -9,7 +10,13 @@ func _ready():
 func _on_timer_timeout() -> void:
 	var random_scene: PackedScene = items.pick_random()
 	var random_item := random_scene.instantiate()
-	add_child(random_item)
+	
+	if spawned_items_count < 6:
+		spawned_items_count += 1
+		add_child(random_item)
+	
+	random_item.tree_exited.connect(_lessen_number_of_spawned_items)
+
 
 	var view_port := get_viewport_rect().size
 	var random_position := Vector2(0, 0)
@@ -17,3 +24,6 @@ func _on_timer_timeout() -> void:
 	random_position.y = randf_range(0, view_port.y)
 	
 	random_item.position = random_position
+
+func _lessen_number_of_spawned_items() -> void:
+	spawned_items_count -= 1
